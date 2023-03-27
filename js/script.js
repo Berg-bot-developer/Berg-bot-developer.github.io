23d4777b9b5b294f79f3ad9b8bac86078445bd4a
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2023-03-27 17:57:40 +0300

    ganter

--- js/script.js
+++ js/script.js
@@ -1,30 +1,4 @@
-const animItems = document.querySelectorAll('_anim-items');
-
-if (animItems.length > 0){
-    window.addEventListener('scroll', animOnScroll);
-    function animOnScroll(Params){
-        for (let index = 0; index < animItems.length; index++) {
-            const animItem = animItems[index];
-            const animItemHeight = animItem.offsetHeight;
-            const animItemOffset = offset(animItem).top;
-            const animStart = 4;
-
-            let animItemPoint = window.innerHeight - animItemHeight / animStart;
-
-            if (animItemHeight > window.innerHeight) {
-                animItemPoint = window.innerHeight - window.innerHeight / animStart;
-            }
-            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-                animItem.classList.add('_animation')
-            } else {
-                animItem.classList.remove('_animation')
-            }
-        }
-    }
-    function offset(el) {
-        const rect = el.getBoundingClientRect(),
-            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
-    }
-}
+function getVisualViewportHeight() {
+    const doc = document.documentElement;
+    doc.style.setProperty('--visual-viewport-height', `${window.visualViewport.height}px`);
+  }
