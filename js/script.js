8918b4a46e48b71a469484ea0a7e7b2ca737e9ee
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2023-03-27 23:49:01 +0300

    neo

--- js/script.js
+++ js/script.js
@@ -2,3 +2,5 @@ function getVisualViewportHeight() {
     const doc = document.documentElement;
     doc.style.setProperty('--visual-viewport-height', `${window.visualViewport.height}px`);
   }
+
+  getVisualViewportHeight();
