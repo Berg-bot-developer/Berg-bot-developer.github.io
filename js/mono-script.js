e803a4e45dbdc51237d5acfdfe0dc9f94b4f7bd2
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2023-04-10 19:19:03 +0300

    comers

--- js/mono-script.js
+++ js/mono-script.js
@@ -1,24 +1,3 @@
-function getVisualViewportHeight() {
-    const doc = document.documentElement;
-    doc.style.setProperty('--visual-viewport-height', `${window.visualViewport.height}px`);
-  }
-
-  getVisualViewportHeight();
-
-  const nav = document.querySelector('#nav');
-  const navBtn = document.querySelector('#nav-btn');
-  const navBtnImg = document.querySelector('#nav-btn-img');
-
-  navBtn.onclick = () => {
-      if (nav.classList.toggle('open')) {
-          navBtnImg.src="Img/NAVCLOSE.svg"
-          navBtn.classList.toggle('_click')
-      } else {
-          navBtnImg.src="Img/menunav.png"
-          navBtn.classList.remove('_click')
-      }
-  }
-
 var popupButton = document.getElementById("popup-button");
 var popup = document.getElementById("popup");
 var closeButton = popup.querySelector(".close-button");
@@ -39,5 +18,3 @@ window.addEventListener("click", function(event) {
     popup.style.display = "none";
   }
 });
-
-
