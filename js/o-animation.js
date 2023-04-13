9d9d23a95fb8f8fbb6830c5126c2aba4f7452bb5
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2023-04-13 22:12:36 +0300

    ura

---
+++ js/o-animation.js
@@ -0,0 +1,16 @@
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('_animation');
+      }
+    });
+  }
+  
+  let options = {
+    threshold: [0.1] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.o-container');
+  
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
